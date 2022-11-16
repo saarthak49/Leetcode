86db60ef3e84ca4cde5a8f6a5475b4f8f201a5cd
Saarthak Prakash <96273846+saarthak49@users.noreply.github.com>
2022-11-16 07:22:03 +0530

    Time: 475 ms (81.55%), Space: 213.3 MB (72.44%) - LeetHub

---
+++ 1824-minimum-sideway-jumps/1824-minimum-sideway-jumps.cpp
@@ -0,0 +1,59 @@
+class Solution {
+public:
+    bool isBlocked(vector<int> &obstacles, int lane, int i)
+    {
+        return obstacles[i] == lane;
+    }
+    int minSideJumps(vector<int>& obstacles) {
+        
+        int n = obstacles.size();
+        vector<int> dp1(n, 1e6);
+        vector<int> dp2(n, 1e6);
+        vector<int> dp3(n, 1e6);
+        
+        
+        dp1[0] = isBlocked(obstacles, 1, 0) ? 1e6 : 1;
+        dp2[0] = isBlocked(obstacles, 2, 0) ? 1e6 : 0;
+        dp3[0] = isBlocked(obstacles, 3, 0) ? 1e6 : 1;
+        
+        for(int i = 1; i<=n - 1; i++)
+        {
+            
+            if(!isBlocked(obstacles, 1, i))
+            {
+                if(!isBlocked(obstacles, 1, i - 1))
+                    dp1[i] = min(dp1[i], dp1[i - 1]);
+                if(!isBlocked(obstacles, 2, i - 1)  && (!isBlocked(obstacles, 1, i - 1) || !isBlocked(obstacles, 2, i)))
+                    dp1[i] = min(dp1[i], 1 + dp2[i - 1]);
+                if(!isBlocked(obstacles, 1, i) && !isBlocked(obstacles, 3, i-1) && (!isBlocked(obstacles, 1, i-1)||!isBlocked(obstacles, 3, i)))
+                   dp1[i] = min(dp1[i], 1 + dp3[i-1]);
+            }
+            
+                   
+            if(!isBlocked(obstacles, 3, i))
+            {
+                if(!isBlocked(obstacles, 3, i - 1))
+                    dp3[i] = min(dp3[i], dp3[i - 1]);
+                if(!isBlocked(obstacles, 2, i - 1)  && (!isBlocked(obstacles, 3, i - 1) || !isBlocked(obstacles, 2, i)))
+                    dp3[i] = min(dp3[i], 1 + dp2[i - 1]);
+                if(!isBlocked(obstacles, 3, i) && !isBlocked(obstacles, 1, i-1) && (!isBlocked(obstacles, 3, i-1)||!isBlocked(obstacles, 1, i)))
+                   dp3[i] = min(dp3[i], 1 + dp1[i-1]);
+            }
+                   
+            if(!isBlocked(obstacles, 2, i))
+            {
+                if(!isBlocked(obstacles, 2, i - 1))
+                    dp2[i] = min(dp2[i], dp2[i - 1]);
+                if(!isBlocked(obstacles, 1, i - 1)  && (!isBlocked(obstacles, 2, i - 1) || !isBlocked(obstacles, 1, i)))
+                    dp2[i] = min(dp2[i], 1 + dp1[i - 1]);
+                if(!isBlocked(obstacles, 3, i - 1)  && (!isBlocked(obstacles, 2, i - 1) || !isBlocked(obstacles, 3, i)))
+                   dp2[i] = min(dp2[i], 1 + dp3[i-1]);
+            }
+                   
+        }
+                   
+        return min(dp2[n-1], min(dp1[n-1], dp3[n-1]));
+        
+    }
+};
+    
