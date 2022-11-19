23773dd7117b7668855009cf57ad0526e78b41ec
Saarthak Prakash <96273846+saarthak49@users.noreply.github.com>
2022-11-20 01:54:29 +0530

    Time: 44 ms (90.77%), Space: 89.9 MB (32.35%) - LeetHub

---
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -0,0 +1,59 @@
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        vector<int> first = nums1.size()<nums2.size()?nums1:nums2;
+        vector<int> second = nums2.size()>nums1.size()?nums2:nums1;
+        
+        
+        int low = 0;
+        int high = first.size();
+        int m = first.size();
+        int n = second.size();
+        // cout<<m<<" "<<n<<endl;
+        int n1 = 0;
+        int n2 = 0;
+        if(m==0 && n==0)
+            return 0;
+        
+        if(m==0)
+        {
+            // cout<<"good till here\n";
+            if(n%2 == 1)
+                return second[(n-1)/2];
+            return (second[n/2] + second[(n-1)/2])/2.0;
+        }
+        
+        while(low <= high)
+        {
+            int i = low + (high - low)/2;
+            int j = (m + n + 1)/2 - i;
+            
+            if(i<=m-1 && j-1>=0 && first[i] < second[j-1])
+                low = i + 1;
+            else if(i-1>=0 && j<=n-1 && first[i-1] > second[j])
+                high = i - 1;
+            else
+            {
+                if(i==0)
+                    n1 = second[j-1];
+                else if(j==0)
+                    n1 = first[i-1];
+                else
+                    n1 = max(first[i-1] , second[j-1]);
+                
+                if(i==m)
+                    n2 = second[j];
+                else if(j==n)
+                    n2 = first[i];
+                else
+                    n2 = min(first[i], second[j]);
+                break;
+            }
+        }
+        
+        if( (m+n) %2 == 1 )
+            return n1;
+        return (n1 + n2)/2.0;
+        
+    }
+};
