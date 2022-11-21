ee03ac07ea51be95676ac9a28a2fea5d1a09b926
Saarthak Prakash <96273846+saarthak49@users.noreply.github.com>
2022-11-21 10:48:31 +0530

    Time: 416 ms (58.65%), Space: 116.7 MB (41.35%) - LeetHub

--- 2359-find-closest-node-to-given-two-nodes/2359-find-closest-node-to-given-two-nodes.cpp
+++ 2359-find-closest-node-to-given-two-nodes/2359-find-closest-node-to-given-two-nodes.cpp
@@ -25,12 +25,23 @@ public:
         return returnPoint;
         
     }
-    void getDfsPath(int source, vector<int> &edges, int mp1[], int i)
+    void getDfsPath(int source, vector<int> &edges, int mp1[], int mp2[], int i, int &minDist, int &returnPoint)
     {
         mp1[source] = i;
+        if(mp2[source] != -1 )
+        {
+            int dist = max(i, mp2[source]);
+            if(dist < minDist)
+            {
+                minDist = dist;
+                returnPoint = source;
+            }
+            else if(dist == minDist && source < returnPoint)
+                returnPoint = source;
+        }
         if(edges[source] != -1 && mp1[edges[source]] == -1)
         {
-            getDfsPath(edges[source], edges, mp1, i+1);
+            getDfsPath(edges[source], edges, mp1, mp2, i+1, minDist, returnPoint);
         }
     }
     
@@ -41,22 +52,25 @@ public:
         int mp1[n];
         memset(mp1, -1, sizeof(mp1));
         
-        vector<int> firstNodeDfs;
-        getDfsPath(node1, edges, mp1, 0);
-        
         int mp2[n];
         memset(mp2, -1, sizeof(mp2));
         
-        vector<int> secondNodeDfs;
+        vector<int> firstNodeDfs;
         int i = 0;
-        getDfsPath(node2, edges, mp2, i);
+        int returnPoint = -1;
+        int minDist = INT_MAX;
+        getDfsPath(node1, edges, mp1, mp2, i, minDist, returnPoint);
+        
+        
+        
+        getDfsPath(node2, edges, mp2, mp1, i, minDist, returnPoint);
         
         
         
         
-        int point = findIntersection(mp1, mp2, n);
+        // int point = findIntersection(mp1, mp2, n);
         
-        return point;
+        return returnPoint;
         
     }
 };
