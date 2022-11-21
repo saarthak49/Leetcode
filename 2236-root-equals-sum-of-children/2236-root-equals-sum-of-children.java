802a034d05042a537a3aecc5a7fbf45f1b1ab0e5
Saarthak Prakash <96273846+saarthak49@users.noreply.github.com>
2022-11-21 16:33:50 +0530

    Time: 1 ms (80.25%), Space: 42.1 MB (23.48%) - LeetHub

--- 2236-root-equals-sum-of-children/2236-root-equals-sum-of-children.java
+++ 2236-root-equals-sum-of-children/2236-root-equals-sum-of-children.java
@@ -15,8 +15,6 @@
  */
 class Solution {
     public boolean checkTree(TreeNode root) {
-        if(root.val == root.left.val + root.right.val)
-            return true;
-        return false;
+        return root.val == root.left.val + root.right.val;
     }
 }
