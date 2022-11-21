90493b83779e8f9e0b033106b48176265f72ab77
Saarthak Prakash <96273846+saarthak49@users.noreply.github.com>
2022-11-21 10:26:37 +0530

    Time: 778 ms (24.69%), Space: 140.5 MB (27.99%) - LeetHub

---
+++ 2359-find-closest-node-to-given-two-nodes/2359-find-closest-node-to-given-two-nodes.cpp
@@ -0,0 +1,74 @@
+class Solution {
+public:
+    
+    
+    int findIntersection(vector<int> vec1, vector<int> vec2)    
+    {
+        unordered_map<int, int> mp1;
+        int minDist = INT_MAX;
+        int returnPoint = -1;
+        
+        for(int i = 0; i<=vec1.size() - 1; i++)
+        {
+            mp1[vec1[i]] = i;
+        }
+        
+        for(int i = 0; i<=vec2.size() - 1; i++)
+        {
+            if(mp1.find(vec2[i]) != mp1.end())
+            {
+                // cout<<"Running for : "<<vec2[i]<<endl;
+                // cout<<"dist : "<<i<<" from map: "<<mp1[vec2[i]]<<endl; 
+                int dist = max(i, mp1[vec2[i]]);
+                if(dist < minDist)
+                {
+                    minDist = dist;
+                    returnPoint = vec2[i];
+                }
+                else if(dist == minDist && vec2[i] < returnPoint)
+                    returnPoint = vec2[i];
+            }
+        }
+        
+        return returnPoint;
+        
+    }
+    void getDfsPath(int source, vector<int> &edges, bool vis[], vector<int> &vec)
+    {
+        vis[source] = true;
+        vec.push_back(source);
+        if(edges[source] != -1 && !vis[edges[source]])
+            getDfsPath(edges[source], edges, vis, vec);
+    }
+    
+    void printVector(vector<int> vec)
+    {
+        for(auto it: vec)
+            cout<<it<<" ";
+        cout<<endl;
+    }
+    
+    int closestMeetingNode(vector<int>& edges, int node1, int node2) {
+        bool vis[100005];
+        memset(vis, false, sizeof(vis));
+        
+        vector<int> firstNodeDfs;
+        getDfsPath(node1, edges, vis, firstNodeDfs);
+        
+        memset(vis, false, sizeof(vis));
+        
+        vector<int> secondNodeDfs;
+        getDfsPath(node2, edges, vis, secondNodeDfs);
+        
+        // printVector(firstNodeDfs);
+        // printVector(secondNodeDfs);
+        
+        
+        
+        int point = findIntersection(firstNodeDfs, secondNodeDfs);
+        
+        return point;
+        // return 0;
+        
+    }
+};
