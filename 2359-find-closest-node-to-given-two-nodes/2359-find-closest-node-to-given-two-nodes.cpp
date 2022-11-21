e6b8ca2dac6304624d85f3b73097909c45b087c2
Saarthak Prakash <96273846+saarthak49@users.noreply.github.com>
2022-11-21 10:53:44 +0530

    Time: 506 ms (41.20%), Space: 116.3 MB (41.35%) - LeetHub

--- 2359-find-closest-node-to-given-two-nodes/2359-find-closest-node-to-given-two-nodes.cpp
+++ 2359-find-closest-node-to-given-two-nodes/2359-find-closest-node-to-given-two-nodes.cpp
@@ -1,33 +1,9 @@
 class Solution {
 public:
     
-    
-    int findIntersection(int mp1[], int mp2[], int n)    
+    void getDfsWithMap(int source, vector<int> &edges, bool vis[], int mp2[], int i, int &minDist, int &returnPoint)
     {
-        int minDist = INT_MAX;
-        int returnPoint = -1;
-        
-        for(int i = 0; i<=n - 1; i++)
-        {
-            if(mp1[i] != -1 && mp2[i] != -1)
-            {
-                int dist = max(mp1[i], mp2[i]);
-                if(dist < minDist)
-                {
-                    minDist = dist;
-                    returnPoint = i;
-                }
-                else if(dist == minDist && i < returnPoint)
-                    returnPoint = i;
-            }
-        }
-        
-        return returnPoint;
-        
-    }
-    void getDfsPath(int source, vector<int> &edges, int mp1[], int mp2[], int i, int &minDist, int &returnPoint)
-    {
-        mp1[source] = i;
+        vis[source] = true;
         if(mp2[source] != -1 )
         {
             int dist = max(i, mp2[source]);
@@ -39,9 +15,18 @@ public:
             else if(dist == minDist && source < returnPoint)
                 returnPoint = source;
         }
+        if(edges[source] != -1 && !vis[edges[source]])
+        {
+            getDfsWithMap(edges[source], edges, vis, mp2, i+1, minDist, returnPoint);
+        }
+    }
+    
+    void justDfs(int source, vector<int> &edges, int mp1[], int i)
+    {
+        mp1[source] = i;
         if(edges[source] != -1 && mp1[edges[source]] == -1)
         {
-            getDfsPath(edges[source], edges, mp1, mp2, i+1, minDist, returnPoint);
+            justDfs(edges[source], edges, mp1, i+1);
         }
     }
     
@@ -50,25 +35,19 @@ public:
         
         int n = edges.size();
         int mp1[n];
+        bool vis[n];
         memset(mp1, -1, sizeof(mp1));
+        memset(vis, false, sizeof(vis));
         
-        int mp2[n];
-        memset(mp2, -1, sizeof(mp2));
         
-        vector<int> firstNodeDfs;
         int i = 0;
         int returnPoint = -1;
+        
+        
         int minDist = INT_MAX;
-        getDfsPath(node1, edges, mp1, mp2, i, minDist, returnPoint);
+        justDfs(node1, edges, mp1, i);
         
-        
-        
-        getDfsPath(node2, edges, mp2, mp1, i, minDist, returnPoint);
-        
-        
-        
-        
-        // int point = findIntersection(mp1, mp2, n);
+        getDfsWithMap(node2, edges, vis, mp1, i, minDist, returnPoint);
         
         return returnPoint;
         
