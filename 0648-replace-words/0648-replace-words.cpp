c747c77243d8b10d1b4b37b923d633c13f2c43ea
Saarthak Prakash <96273846+saarthak49@users.noreply.github.com>
2022-11-21 08:39:13 +0530

    Time: 1049 ms (12.30%), Space: 386 MB (5.09%) - LeetHub

--- 0648-replace-words/0648-replace-words.cpp
+++ 0648-replace-words/0648-replace-words.cpp
@@ -2,10 +2,8 @@ class Solution {
 public:
     string replaceWords(vector<string>& dictionary, string sentence) {
         unordered_map<string, int> mp;
-        
         for(auto it: dictionary)
             mp[it]++;
-        
         int n = sentence.size();
         string word = "";
         string root = "";
@@ -25,11 +23,7 @@ public:
             word += sentence[i];
             
             if(mp[word] != 0 && root == "")
-            {
-                // cout<<"The word so far is : "<<word<<endl;
-                // cout<<"The identified root is: "<<word<<endl;
                 root = word;
-            }
         }
         n = finalString.size();
         return finalString.substr(0, n-1);
