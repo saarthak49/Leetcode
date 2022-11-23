9f6832937819f86f421e5af27d082df528269a7c
Saarthak Prakash <96273846+saarthak49@users.noreply.github.com>
2022-11-23 06:41:59 +0530

    Time: 11 ms (88.00%), Space: 7.7 MB (17.07%) - LeetHub

---
+++ 0812-largest-triangle-area/0812-largest-triangle-area.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    
+    double getDeterminant(int x1, int x2, int y1, int y2)
+    {
+        return abs((x1 * y2 - x2 * y1)/2.0);
+    }
+    
+    double largestTriangleArea(vector<vector<int>>& points) {
+        double largestArea = 0;
+        int n = points.size();
+        for(int i = 0; i<=n-3; i++)
+        {
+            for(int j = i + 1; j<=n-2; j++)
+            {
+                for(int k = j + 1; k<=n-1; k++)
+                {
+                    int x1 = points[j][0] - points[i][0];
+                    int x2 = points[k][0] - points[j][0];
+                    
+                    int y1 = points[j][1] - points[i][1];
+                    int y2 = points[k][1] - points[j][1];
+                    
+                    largestArea = max(largestArea, getDeterminant(x1, x2, y1, y2));
+                }
+            }
+        }
+        return largestArea;
+        
+    }
+};
