0985259f26c037a89e08a9fd4270c54980ab9612
Saarthak Prakash <96273846+saarthak49@users.noreply.github.com>
2022-11-23 06:34:07 +0530

    Time: 28 ms (48.16%), Space: 11.4 MB (11.65%) - LeetHub

---
+++ 1089-duplicate-zeros/1089-duplicate-zeros.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    
+    unordered_map<int, int> findElementAtTail(vector<int> &arr)
+    {
+        int count = 0;
+        int n = arr.size();
+        unordered_map<int, int> mp;
+        for(int i = 0; i<=n - 1; i++)
+        {
+            
+            mp[i] = i + count;
+            if(arr[i] == 0)
+                count++;
+        }
+        return mp;
+    }
+    
+    void duplicateZeros(vector<int>& arr) {
+        unordered_map<int, int> mp = findElementAtTail(arr);
+        int n = arr.size();
+        int j = n-1;
+        // for(auto it: mp)
+        // {
+        //     cout<<it.first<<" "<<it.second<<endl;
+        // }
+        for(int i = n-1; i>=0; i--)
+        {
+            if(mp[i] <= n-1)
+            {
+                // cout<<arr[i]<<" being moved to position "<<mp[i]<<endl;
+                arr[mp[i]] = arr[i];
+                if(arr[i] == 0 && mp[i] + 1 <= n-1)
+                    arr[mp[i] + 1] = 0;
+            }
+        }
+    }
+};
