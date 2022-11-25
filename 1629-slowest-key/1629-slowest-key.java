0004a25537ad665c5e7e26b98205ea9a5505b038
Saarthak Prakash <96273846+saarthak49@users.noreply.github.com>
2022-11-25 14:32:08 +0530

    Time: 1 ms (97.69%), Space: 42.3 MB (90.56%) - LeetHub

--- 1629-slowest-key/1629-slowest-key.java
+++ 1629-slowest-key/1629-slowest-key.java
@@ -1,10 +1,9 @@
 class Solution {
-public:
-    char slowestKey(vector<int>& releaseTimes, string keysPressed) {
+    public char slowestKey(int[] releaseTimes, String keysPressed) {
         int last = 0;
         int largestDuration = 0;
         char maxDurationKey = 'z';
-        int n = keysPressed.size();
+        int n = keysPressed.length();
         for(int i = 0; i<=n-1; i++)
         {
             int duration = releaseTimes[i] - last;
@@ -13,14 +12,15 @@ public:
             if(duration > largestDuration)
             {
                 largestDuration = duration;
-                maxDurationKey = keysPressed[i];
+                maxDurationKey = keysPressed.charAt(i);
             }
             else if(duration == largestDuration)
             {
-                if(keysPressed[i] > maxDurationKey)
-                    maxDurationKey = keysPressed[i];
+                if(keysPressed.charAt(i) > maxDurationKey)
+                    maxDurationKey = keysPressed.charAt(i);
             }
         }
         return maxDurationKey;
+        
     }
-};
+}
