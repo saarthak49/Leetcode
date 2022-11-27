1a64e37f4dd5de167324cce2cb07afb7b288a8f0
Saarthak Prakash <96273846+saarthak49@users.noreply.github.com>
2022-11-27 17:28:18 +0530

    Time: 533 ms (14.02%), Space: 150.1 MB (22.56%) - LeetHub

---
+++ 0446-arithmetic-slices-ii-subsequence/0446-arithmetic-slices-ii-subsequence.java
@@ -0,0 +1,56 @@
+class Solution {
+    public int numberOfArithmeticSlices(int[] nums) {
+        int n = nums.length;
+        
+        Map<Long, Long> mp = new HashMap<>();
+        mp.put(0L, 0L);
+        
+        List<Map<Long, Long> > dp = new ArrayList<>();
+        
+        for(int i = 0; i<=n-1; i++)
+            dp.add(mp);
+        
+        
+        dp.set(0, mp);
+        
+        for(int i = 1; i<=n-1; i++)
+        {
+            Map<Long, Long> tempMap = new HashMap<>();
+            for(int j = 0; j<=i-1; j++)
+            {
+                long cd = (long)nums[i] - nums[j];
+                long prev = 0;
+                long curr = 0;
+                if(dp.get(j).containsKey(cd))
+                {
+                    // System.out.println(dp.get(j).get(cd));
+                    prev = dp.get(j).get(cd);
+                }
+                
+                
+                if(tempMap.containsKey(cd))
+                    curr = tempMap.get(cd);
+                
+                tempMap.put(cd, prev + curr + 1);
+                
+                // System.out.println(j + " " + i + " " + cd + " " + prev + " " + curr + " " + dp.get(i).get(cd));
+            }
+            dp.set(i, tempMap);
+        }
+        
+        long sum = 0;
+        for(Map<Long, Long> te: dp)
+        {
+            for(long val: te.values())
+            {
+                // System.out.println(val);
+                sum += val;
+            }
+        }
+        
+        
+        return (int)(sum - (long)n * (n-1)/2);
+        
+        
+    }
+}
