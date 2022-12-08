edfa8159fbfdc11cb745687252dd0b6c36603b28
Saarthak Prakash <96273846+saarthak49@users.noreply.github.com>
2022-12-08 07:16:07 +0530

    Time: 122 ms (77.78%), Space: 79.6 MB (87.13%) - LeetHub

---
+++ 1483-kth-ancestor-of-a-tree-node/1483-kth-ancestor-of-a-tree-node.java
@@ -0,0 +1,65 @@
+class TreeAncestor {
+    
+    
+    private int[][] sparseTable;
+    private int depth;
+
+    public TreeAncestor(int n, int[] parent) {
+        
+        depth = (int)Math.floor(Math.log(n)/Math.log(2));
+        
+        sparseTable = new int[n][depth + 1];
+        
+        
+        for(int j = 0; j<=depth; j++)
+        {
+            for(int i = 0; i<=n-1; i++)
+            {
+                if(i == 0)
+                {
+                    sparseTable[i][j] = -1;
+                    continue;
+                }
+                
+                
+                if(j == 0)
+                {
+                    sparseTable[i][j] = parent[i];
+                    continue;
+                }
+                
+                
+                
+                if(sparseTable[i][j - 1] != -1)
+                    sparseTable[i][j] = sparseTable[sparseTable[i][j-1]][j-1];
+                else
+                    sparseTable[i][j] = -1;
+            }
+        }
+        
+    }
+    
+    public int getKthAncestor(int node, int k) {
+        
+        if(node == 0)
+            return -1;
+        
+        int ancestor = node;
+        if((int)Math.floor(Math.log(k)/Math.log(2)) > depth)
+            return -1;
+        
+        while(k >= 1 && ancestor != -1)
+        {
+            int colNumber = (int)Math.floor(Math.log(k)/Math.log(2));
+            ancestor = this.sparseTable[ancestor][colNumber];
+            k -= Math.pow(2, colNumber);
+        }
+        return ancestor;
+    }
+}
+
+/**
+ * Your TreeAncestor object will be instantiated and called as such:
+ * TreeAncestor obj = new TreeAncestor(n, parent);
+ * int param_1 = obj.getKthAncestor(node,k);
+ */
