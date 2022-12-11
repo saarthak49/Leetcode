048c954a929a3cbb199e45bc93ea5dbf2d05da03
Saarthak Prakash <96273846+saarthak49@users.noreply.github.com>
2022-12-12 03:19:09 +0530

    Time: 1 ms (89.23%), Space: 41.8 MB (69.85%) - LeetHub

---
+++ 2027-minimum-moves-to-convert-string/2027-minimum-moves-to-convert-string.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int minimumMoves(String s) {
+        
+        int n = s.length();
+        int count = 0;
+        
+        
+        int i = 0;
+        while(i <= n-1)
+        {
+            if(s.charAt(i) == 'X')
+            {
+                count++;
+                i+=3;
+            }
+            else
+                i++;
+        }
+        
+        return count;
+    }
+}
