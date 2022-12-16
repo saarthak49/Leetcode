de213c22afd530c3156cb8a6d9e7c47b62f90121
Saarthak Prakash <96273846+saarthak49@users.noreply.github.com>
2022-12-16 12:45:17 +0530

    Time: 8 ms (59.61%), Space: 41 MB (97.09%) - LeetHub

--- 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.java
+++ 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.java
@@ -8,32 +8,30 @@ class Solution {
         int n = s.length();
         int m = part.length();
         
+        StringBuilder st1 = new StringBuilder("");
+        for(int i = 1; i<=1000; i++)
+            st1.append("a");
+        
+        int index = 0;
+        
+        
         for(int i = 0; i<=n-1; i++)
         {
-            st.add(s.charAt(i));
+            st1.append(s.charAt(i));
+            st1.setCharAt(index, s.charAt(i));
             
             int j = m - 1;
-            Stack<Character> temp = new Stack<>();
-            while(j >= 0 && !st.isEmpty() && st.peek() == part.charAt(j))
+            int temp = index;
+            while(j >= 0 && temp >=0 && st1.charAt(temp) == part.charAt(j))
             {
-                temp.add(st.pop());
+                temp--;
                 j--;
             }
-            if(j != -1)
-            {
-                while(!temp.isEmpty())
-                    st.add(temp.pop());
-            }
+            if(j == -1)
+                index = temp;
+            
+            index++;
         }
-        StringBuilder str = new StringBuilder("");
-        
-        while(!st.isEmpty())
-        {
-            str.append(st.pop());
-        }
-        
-        return String.valueOf(str.reverse());
-        
-        
+        return st1.substring(0, index);
     }
 }
