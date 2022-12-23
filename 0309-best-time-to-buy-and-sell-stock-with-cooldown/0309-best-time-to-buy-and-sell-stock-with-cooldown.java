3ceefd36ad426edd889a934b596fcb1684fae104
Saarthak Prakash <96273846+saarthak49@users.noreply.github.com>
2022-12-23 23:50:40 +0530

    Time: 0 ms (100.00%), Space: 40.1 MB (97.50%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -4,26 +4,22 @@ class Solution {
         int n = prices.length;
         if(n <= 1)
             return 0;
-        int[] dp1 = new int[n];
-        int[] dp2 = new int[n];
-        
-        
-        dp1[0] = -prices[0];
-        dp2[0] = 0;
-        
-        
-        dp1[1] = Math.max(-prices[1], dp1[0]);
-        dp2[1] = Math.max(prices[1] - prices[0], 0);
+        int a = -prices[0];
+        int b = 0;
+        int c = Math.max(0, prices[1] - prices[0]);
         
         
         
             
-        for(int i = 2; i<=n-1; i++)
+        for(int i = 1; i<=n-1; i++)
         {
-            dp1[i] = Math.max(dp1[i-1], dp2[i-2] - prices[i]);
-            dp2[i] = Math.max(dp2[i-1], dp1[i-1] + prices[i]);
+            int t1 = Math.max(a, c - prices[i]);
+            int t2 = Math.max(b, a + prices[i]);
+            c = b;
+            b = t2;
+            a = t1;
         }
-        return dp2[n-1] < 0 ? 0 : dp2[n-1];
+        return b;
         
     }
 }
