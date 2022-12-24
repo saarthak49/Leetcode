ef193f08f6ff97f2f1a6c99f2536a90a2d682063
Saarthak Prakash <96273846+saarthak49@users.noreply.github.com>
2022-12-24 13:23:07 +0530

    Time: 1 ms (90.34%), Space: 39.3 MB (82.13%) - LeetHub

--- 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.java
+++ 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.java
@@ -6,17 +6,12 @@ class Solution {
     public int numTilings(int n) {
         
         
-        int[] h = new int[n + 1];
         int[] sum = new int[n + 1];
         if(n == 1)
             return 1;
         else if(n == 2)
             return 2;
         
-        
-        h[0] = 1;
-        h[1] = 1;
-        h[2] = 2;
         
         sum[0] = 1;
         sum[1] = 2;
