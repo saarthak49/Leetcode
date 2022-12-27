427c928fd2d2848ee1c73addcbc7eb0e315043a4
Saarthak Prakash <96273846+saarthak49@users.noreply.github.com>
2022-12-27 19:45:21 +0530

    Time: 16 ms (76.11%), Space: 52.9 MB (81.12%) - LeetHub

--- 2279-maximum-bags-with-full-capacity-of-rocks/2279-maximum-bags-with-full-capacity-of-rocks.java
+++ 2279-maximum-bags-with-full-capacity-of-rocks/2279-maximum-bags-with-full-capacity-of-rocks.java
@@ -3,22 +3,21 @@ class Solution {
         
         
         int n = capacity.length;
-        int[] rem = new int[n];
         
         for(int i = 0; i<=n-1; i++)
-            rem[i] = capacity[i] - rocks[i];
+            rocks[i] = capacity[i] - rocks[i];
         
         
-        Arrays.sort(rem);
+        Arrays.sort(rocks);
         int count = 0;
         for(int i = 0; i<=n-1 && additionalRocks > 0; i++)
         {
-            if(rem[i] == 0)
+            if(rocks[i] == 0)
                 count++;
-            else if(rem[i] > 0 && rem[i] <= additionalRocks)
+            else if(rocks[i] > 0 && rocks[i] <= additionalRocks)
             {
                 count++;
-                additionalRocks -= rem[i];
+                additionalRocks -= rocks[i];
             }
         }
         
