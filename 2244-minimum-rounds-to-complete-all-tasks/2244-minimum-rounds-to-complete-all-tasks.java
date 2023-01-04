50eae93d7db55605dd80a2c6a18d7c015c5a9a72
Saarthak Prakash <96273846+saarthak49@users.noreply.github.com>
2023-01-04 09:38:43 +0530

    Time: 86 ms (41.04%), Space: 108.9 MB (15.13%) - LeetHub

--- 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.java
+++ 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.java
@@ -1,30 +1,14 @@
 class Solution {
-    
-    
-    private int[] dp;
-    
-    Solution()
-    {
-        this.dp = new int[(int)1e5 + 1];
-        dp[1] = 1;
-        for(int i = 2; i<=(int)1e5; i++)
-            dp[i] = Math.min(dp[i-2], (i-3)>=0?dp[i-3]:0) + 1;
-    }
-    
-    
     public int minimumRounds(int[] tasks) {
         Map<Integer, Integer> mp = new HashMap<>();
-        
         for(int task : tasks)
             mp.put(task, mp.getOrDefault(task,0)+1);
-        
         int count = 0;
         for(int value : mp.values())
         {
             if(value == 1)
                 return -1;
-            else
-                count += dp[value];
+            count += (value + 2)/3;
         }
         return count;
     }
