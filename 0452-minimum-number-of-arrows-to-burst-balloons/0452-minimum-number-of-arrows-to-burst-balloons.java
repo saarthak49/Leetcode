8c29e22f51184efb697f9dd5b6767202986b6ed4
Saarthak Prakash <96273846+saarthak49@users.noreply.github.com>
2023-01-05 22:30:31 +0530

    Time: 131 ms (25.96%), Space: 101.2 MB (10.21%) - LeetHub

--- 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.java
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.java
@@ -3,6 +3,7 @@ class Solution {
         Arrays.sort(points, (a, b) -> Integer.compare(a[0], b[0]));
         int arrows = 1;
         int n = points.length;
+        int last = points[0][1];
         for(int i = 1; i<=n-1; i++)
         {
             if(points[i][0] >= points[i-1][0] && points[i][0] <= points[i-1][1])
