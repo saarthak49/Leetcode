9c1b6619ae503d6bf5f3ab647ae76ef0955abe8d
Saarthak Prakash <96273846+saarthak49@users.noreply.github.com>
2023-01-24 01:44:39 +0530

    Time: 4 ms (55.24%), Space: 75 MB (5.63%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.java
+++ 0997-find-the-town-judge/0997-find-the-town-judge.java
@@ -1,17 +1,17 @@
 class Solution {
     public int findJudge(int n, int[][] trust) {
-        Map<Integer, Integer> backward = new HashMap<>();
-        Map<Integer, Integer> forward = new HashMap<>();
+        int[] fw = new int[n+1];
+        int[] bw = new int[n+1];
         for(int[] tr : trust)
         {
             int a = tr[0];
             int b = tr[1];
-            backward.put(b, backward.getOrDefault(b, 0) + 1);
-            forward.put(a, forward.getOrDefault(a, 0) + 1);
+            fw[b]++;
+            bw[a]++;
         }
         for(int i = 1; i<=n; i++)
         {
-            if(backward.getOrDefault(i, 0) == n-1 && forward.getOrDefault(i, 0) == 0)
+            if(fw[i] == n-1 && bw[i] == 0)
                 return i;
         }
         return -1;
